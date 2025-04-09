9ec3450927104fa3347f1b6be4a32e760bdd28e6
JunMinGeun <80248856+Jun-min-geun@users.noreply.github.com>
2025-04-10 04:33:37 +0900

    feat: 테스트페이지 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/RightPages/Home/Home";
 import MyPage from "./pages/RightPages/MyPage/MyPage";
+import Test from "./pages/RightPages/Test/Test";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/test" element={<Test />} />
         <Route path="/mypage" element={<MyPage />} />
       </Routes>
     </BrowserRouter>
