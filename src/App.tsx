c0db4af024bcb691cbfc030441829bbee139dcff
JunMinGeun <80248856+Jun-min-geun@users.noreply.github.com>
2025-04-11 02:16:38 +0900

    refactor: Layout별 페이지 구분 리팩토링

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./pages/RightPages/Home/Home";
+import Home from "./pages/BasicPages/Home/Home";
 import SettingPage from "./pages/RightPages/MyPage/SettingPage/SettingPage";
-import Test from "./pages/RightPages/Test/Test";
+import Test from "./pages/BasicPages/Test/Test";
+// import ChatDetailPage from "./pages/RightPages/ChatPage/Detail/ChatDetailPage";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/test" element={<Test />} />
         <Route path="/mypage" element={<SettingPage />} />
+        {/* <Route path="/chat/id" element={<ChatDetailPage />} /> */}
       </Routes>
     </BrowserRouter>
   );
