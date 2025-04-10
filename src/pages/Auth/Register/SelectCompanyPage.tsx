7c1e12fa624f9d4811a8d40ac97ee758769c01a7
hhhan-jh <jeo4924@gmail.com>
2025-04-11 03:48:23 +0900

    Feat: 선호 기업 선택 페이지 디자인 완료

--- src/pages/Auth/Register/SelectCompanyPage.tsx
+++ src/pages/Auth/Register/SelectCompanyPage.tsx
@@ -1,30 +1,31 @@
 import { useNavigate } from "react-router-dom";
 import AuthLayout from "../../../layouts/AuthLayout/AuthLayout";
 import BaseLayout from "../../../layouts/BaseLayout/BaseLayout";
-import styles from "./SelectLanguagePage.module.css";
+import styles from "./SelectCompanyPage.module.css";
 
 //component
 import Button from "../../../components/Button/Button";
 
 const languageList = [
-  "Python",
-  "C/C++",
-  "JavaScript",
-  "C#",
-  "Go",
-  "Fortran",
-  "Delphi/Object Pascal",
-  "SQL",
-  "MATLAB",
-  "Rust",
-  "R",
-  "Ruby",
+  "삼성전자",
+  "네이버",
+  "카카오",
+  "라인플러스",
+  "우아한형제들",
+  "구글",
+  "애플",
+  "아마존",
+  "마이크로소프트",
+  "메타",
+  "하이퍼커넥트",
+  "비바리퍼블리카",
+  "당근마켓",
 ];
-const SelectLanguagePage = () => {
+const SelectCompanyPage = () => {
   const navigate = useNavigate();
 
   const handleOnClickNext = () => {
-    navigate("/register/company");
+    navigate("/");
   };
 
   return (
@@ -37,13 +38,11 @@ const SelectLanguagePage = () => {
           {/* 캐치프레이즈 */}
           <div className={styles.catchphrase}>
             <p>Welcome, creator.</p>
-            <p>Tell me about yourself.</p>
+            <p>Almost done.</p>
           </div>
 
           {/* 설명 문구 */}
-          <p className={styles.explain}>
-            선호하는 프로그래밍 언어를 선택해주세요.
-          </p>
+          <p className={styles.explain}>선호하는 기업을 선택해주세요.</p>
 
           {/* 언어 선택 버튼 */}
           <div className={styles.languageButtons}>
@@ -61,4 +60,4 @@ const SelectLanguagePage = () => {
     </BaseLayout>
   );
 };
-export default SelectLanguagePage;
+export default SelectCompanyPage;
