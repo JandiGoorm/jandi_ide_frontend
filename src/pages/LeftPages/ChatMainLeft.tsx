807b3515dd99599ccb523442d4311a49e2b7bb8e
hhhan-jh <jeo4924@gmail.com>
2025-04-12 03:41:56 +0900

    Feat: 채팅 메인페이지 사이드바 디자인 완료
    아직 채팅방 선택 버튼은 컴포넌트화 못해서 나중에 작업할 예정

--- src/pages/LeftPages/ChatMainLeft.tsx
+++ src/pages/LeftPages/ChatMainLeft.tsx
@@ -2,32 +2,61 @@ import clsx from "clsx";
 import { FaLongArrowAltLeft, FaLongArrowAltRight } from "react-icons/fa";
 import { GiHamburgerMenu } from "react-icons/gi";
 import { Sidebar, useSidebar } from "../../layouts/SidebarLayout/SidebarLayout";
-import styles from "./LeftSide.module.css";
+import styles from "./ChatMainLeft.module.css";
+import LogoDemo from "../../../public/LogoDemo.png";
 
 const ChatMainLeft = () => {
   const { isOpen } = useSidebar();
 
   return (
-    <div className={clsx(styles.sidebar, isOpen && styles.open)}>
-      <div className={styles.icon_box}>
-        <GiHamburgerMenu size={28} />
+    <div className={styles.sidebar}>
+      <div className={clsx(styles.top_section, isOpen && styles.open)}>
+        <div className={styles.menu}>
+          <div className={styles.icon_box}>
+            <GiHamburgerMenu size={28} />
+          </div>
+
+          {isOpen && (
+            <div className={styles.logo_box}>
+              <img src={LogoDemo} alt="Logo" className={styles.logo} />
+            </div>
+          )}
+        </div>
+
+        <Sidebar.ToggleButton className={styles.toggle_button}>
+          {(isOpen) =>
+            isOpen ? (
+              <div className={styles.icon_box}>
+                <FaLongArrowAltLeft size={20} />
+                <p>접기</p>
+              </div>
+            ) : (
+              <div className={styles.icon_box}>
+                <FaLongArrowAltRight size={20} />
+                <p>열기</p>
+              </div>
+            )
+          }
+        </Sidebar.ToggleButton>
       </div>
 
-      <Sidebar.ToggleButton className={styles.toggle_button}>
-        {(isOpen) =>
-          isOpen ? (
-            <div className={styles.icon_box}>
-              <FaLongArrowAltLeft size={20} />
-              <p>접기</p>
+      {isOpen && (
+        <div className={styles.container}>
+          <div className={styles.title_div}>
+            <div className={styles.title}>Welcome to</div>
+            <div className={styles.title}>Chat page!</div>
+            <div className={styles.sub_title}>채팅방을 선택해주세요.</div>
+          </div>
+
+          <button className={styles.chat_button}>
+            <p className={styles.chat_name}>네이버 오픈 채팅</p>
+            <div className={styles.chat_status}>
+              <div className={styles.status_dot}></div>
+              <p className={styles.chat_explain}>2321명 소통중</p>
             </div>
-          ) : (
-            <div className={styles.icon_box}>
-              <FaLongArrowAltRight size={20} />
-              <p>열기</p>
-            </div>
-          )
-        }
-      </Sidebar.ToggleButton>
+          </button>
+        </div>
+      )}
     </div>
   );
 };
