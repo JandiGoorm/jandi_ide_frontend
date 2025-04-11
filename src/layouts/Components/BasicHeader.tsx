2655199200c382e51b9ca9e6e7fbd753f1e88bc3
Junmingeun <junminguen@naver.com>
2025-04-11 15:56:34 +0900

    feat: 다크모드버튼 디자인 수정 및 위치 수정

--- src/layouts/Components/BasicHeader.tsx
+++ src/layouts/Components/BasicHeader.tsx
@@ -1,6 +1,7 @@
 import styles from "./BasicHeader.module.css";
 import React from "react";
 import { useDarkModeContext } from "../../contexts/DarkmodeContext";
+import { FaSun, FaMoon } from "react-icons/fa";
 
 const BasicHeader: React.FC = () => {
   const { isDarkMode, toggleDarkMode } = useDarkModeContext();
@@ -12,9 +13,14 @@ const BasicHeader: React.FC = () => {
           className={`${styles.switch} ${isDarkMode ? styles.switchOn : ""}`}
           onClick={toggleDarkMode}
         >
-          <div className={styles.switchHandle}></div>
+          <div className={styles.switchHandle}>
+            {isDarkMode ? (
+              <FaMoon size={16} color={"white"} />
+            ) : (
+              <FaSun size={16} color={"white"} />
+            )}
+          </div>
         </div>
-        <span>{isDarkMode ? "On" : "Off"}</span>
       </div>
     </div>
   );
