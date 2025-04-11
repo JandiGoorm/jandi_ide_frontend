ef8ccc3733bc484bac7faf96cf850dd92670738f
Junmingeun <junminguen@naver.com>
2025-04-11 18:04:07 +0900

    feat: 채팅페이지 header 생성

---
+++ src/layouts/Components/ChatHeader.tsx
@@ -0,0 +1,29 @@
+import styles from "./ChatHeader.module.css";
+import React from "react";
+import { useDarkModeContext } from "../../contexts/DarkmodeContext";
+import { FaSun, FaMoon } from "react-icons/fa";
+
+const ChatHeader: React.FC = () => {
+  const { isDarkMode, toggleDarkMode } = useDarkModeContext();
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.switchContainer}>
+        <div
+          className={`${styles.switch} ${isDarkMode ? styles.switchOn : ""}`}
+          onClick={toggleDarkMode}
+        >
+          <div className={styles.switchHandle}>
+            {isDarkMode ? (
+              <FaMoon size={16} color={"white"} />
+            ) : (
+              <FaSun size={16} color={"white"} />
+            )}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ChatHeader;
