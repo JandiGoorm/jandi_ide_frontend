541af3bd0d9cf753378f708bd8c36dbe8b390b1c
JunMinGeun <80248856+Jun-min-geun@users.noreply.github.com>
2025-04-17 05:27:16 +0900

    feat: axiosinstance 설정 및 인터셉터 헤더 설정 구현

---
+++ src/apis/axiosInstance.ts
@@ -0,0 +1,31 @@
+import axios from "axios";
+import { APIEndPoints } from "../constants/api";
+
+const BASE_URL = import.meta.env.VITE_BASE_URL;
+
+const axiosInstance = axios.create({
+  baseURL: BASE_URL,
+});
+
+const useHeaderEndPoints = new Set([`GET:${APIEndPoints.MY_INFO}`]);
+
+axiosInstance.interceptors.request.use((config) => {
+  const method = config.method?.toUpperCase();
+  const url = config.url;
+  const requestKey = `${method}:${url}`;
+
+  console.log("interceptor", { method, url, requestKey });
+
+  const isRequiredAuth = useHeaderEndPoints.has(requestKey);
+
+  console.log(isRequiredAuth);
+
+  if (isRequiredAuth) {
+    console.log("여기");
+    const accessToken = localStorage.getItem("accessToken") ?? "";
+    config.headers["Authorization"] = `Bearer ${accessToken}`;
+  }
+
+  return config;
+});
+export default axiosInstance;
