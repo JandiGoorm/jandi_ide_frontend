fae9ac96597c44f7cde3f1743e65f55fe83260a6
Junmingeun <junminguen@naver.com>
2025-04-18 16:12:05 +0900

    feat: 프로젝트디테일페이지 api 연결

--- src/pages/RightPages/CodeEditPage/ProjectEditPage/ProjectEditPage.tsx
+++ src/pages/RightPages/CodeEditPage/ProjectEditPage/ProjectEditPage.tsx
@@ -4,41 +4,26 @@ import { Sidebar } from "../../../../layouts/SidebarLayout/SidebarLayout";
 import LeftSide from "../../../LeftPages/CodeEditPage/ProjectLeft";
 import Editor from "@monaco-editor/react";
 import { useDarkModeContext } from "../../../../contexts/DarkmodeContext";
-import useAxios from "../../../../hooks/useAxios";
 import { useEffect, useState } from "react";
-import { buildPath } from "../../../../utils/buildPath";
-import { APIEndPoints } from "../../../../constants/api";
 import { useParams } from "react-router-dom";
-import { Tree } from "../../../../constants/types/types";
+import useProjectDetails from "../../../../hooks/useProjectDetails";
 
 const ProjectEditPage = () => {
   const { isDarkMode } = useDarkModeContext();
-  const { fetchData: getApi } = useAxios();
-  const { fetchData: getCodeApi } = useAxios();
   const { id } = useParams<{ id: string }>();
-  const [fileTree, setFileTree] = useState<Tree[]>([]);
+  const numericId = id ? Number(id) : undefined;
+  const { projectFileTree, fileContent, getProjectCode } = useProjectDetails({
+    id: numericId as number,
+  });
   const [selectedFile, setSelectedFile] = useState<string | null>(null);
-  const [fileContent, setFileContent] = useState<string>("");
-
-  useEffect(() => {
-    if (!id) return;
-
-    getApi({
-      method: "GET",
-      url: buildPath(APIEndPoints.MANAGE_PROJECT, { id }),
-    }).then((res) => {
-      console.log(res?.data);
-      setFileTree(res?.data.tree);
-    });
-  }, [getApi]);
 
   useEffect(() => {
     if (!id || !selectedFile) return;
 
-    console.log(fileTree);
+    console.log(projectFileTree);
     console.log(selectedFile);
 
-    const matchedFile = fileTree.find((file) => {
+    const matchedFile = projectFileTree.find((file) => {
       const fileName = file.path.split("/").pop();
       return fileName === selectedFile;
     });
@@ -46,29 +31,21 @@ const ProjectEditPage = () => {
     console.log(matchedFile);
     if (!matchedFile) return;
 
-    getCodeApi({
-      method: "GET",
-      url: buildPath(APIEndPoints.PROJECT_BLOB, { id }),
-      params: {
-        sha: matchedFile.sha,
-      },
-    }).then((res) => {
-      const encodedContent = res?.data.content;
-      const encoding = res?.data.encoding;
+    const fethchCodes = async (sha: string) => {
+      if (!sha) return;
 
-      if (encoding === "base64" && encodedContent) {
-        const decoded = atob(encodedContent);
-        setFileContent(decoded);
-      }
-    });
-  }, [selectedFile, id, getCodeApi]);
+      await getProjectCode({ sha });
+    };
+
+    fethchCodes(matchedFile.sha);
+  }, [selectedFile, id, getProjectCode]);
 
   return (
     <BaseLayout>
       <Sidebar.Provider className={styles.Code_layout}>
         <Sidebar.Panel>
           <LeftSide
-            fileTree={fileTree}
+            fileTree={projectFileTree}
             selectedFile={selectedFile}
             setSelectedFile={setSelectedFile}
           />
