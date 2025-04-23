d1214902b1a0223520e06ae5e1f08a7e69774a1c
JunMinGeun <80248856+Jun-min-geun@users.noreply.github.com>
2025-04-23 17:57:11 +0900

    feat: 검색어 바 추가

--- src/layouts/Components/ChatHeader.tsx
+++ src/layouts/Components/ChatHeader.tsx
@@ -2,12 +2,22 @@ import styles from "./ChatHeader.module.css";
 import React from "react";
 import { useDarkModeContext } from "../../contexts/DarkmodeContext";
 import { FaSun, FaMoon } from "react-icons/fa";
+import Input from "../../components/Input/Input";
+import Button from "../../components/Button/Button";
 
 const ChatHeader: React.FC = () => {
   const { isDarkMode, toggleDarkMode } = useDarkModeContext();
 
   return (
     <div className={styles.container}>
+      <div className={styles.Input_div}>
+        <Input
+          style={{ width: "80%" }}
+          inputSize="sm"
+          placeholder="검색할 단어를 입력하세요"
+        />
+        <Button size="md">검색하기</Button>
+      </div>
       <div className={styles.switchContainer}>
         <div
           className={`${styles.switch} ${isDarkMode ? styles.switchOn : ""}`}
