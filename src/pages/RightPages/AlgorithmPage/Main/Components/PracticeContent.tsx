4fe1f6baea02826a78cfac391e7a2b8df223725a
JunMinGeun <80248856+Jun-min-geun@users.noreply.github.com>
2025-04-24 04:16:59 +0900

    feat: 문제 선택시 리스트 변경

--- src/pages/RightPages/AlgorithmPage/Main/Components/PracticeContent.tsx
+++ src/pages/RightPages/AlgorithmPage/Main/Components/PracticeContent.tsx
@@ -37,7 +37,11 @@ const PracticeContent: React.FC<PracticeContentProps> = ({
               <Button variant="none" onClick={() => toggleProblem(problem)}>
                 <MdClose size={18} />
               </Button>
-              <FaMedal color={getMedalColor(problem.level)} size={18} />
+              <FaMedal
+                color={getMedalColor(problem.level)}
+                size={18}
+                style={{ flexShrink: 0, minWidth: 18 }}
+              />
               <span className={styles.desc}>{problem.title}</span>
               <div className={styles.tag_box}>
                 {problem.tags.map((tag, index) => (
@@ -53,24 +57,32 @@ const PracticeContent: React.FC<PracticeContentProps> = ({
       <div className={styles.select_box}>
         <div className={styles.header}>문제 리스트</div>
         <div className={styles.content_box}>
-          {problems.map((problem) => (
-            <div className={styles.problem} key={problem.id}>
-              <Button variant="none" onClick={() => toggleProblem(problem)}>
-                <FaPlus size={18} />
-              </Button>
-              <div>
-                <FaMedal color={getMedalColor(problem.level)} size={18} />
+          {problems
+            .filter(
+              (problem) => !selectedProblems.some((p) => p.id === problem.id)
+            ) // 선택된 문제 제외
+            .map((problem) => (
+              <div className={styles.problem} key={problem.id}>
+                <Button variant="none" onClick={() => toggleProblem(problem)}>
+                  <FaPlus size={18} />
+                </Button>
+                <div>
+                  <FaMedal
+                    color={getMedalColor(problem.level)}
+                    size={18}
+                    style={{ flexShrink: 0, minWidth: 18 }}
+                  />
+                </div>
+                <span className={styles.desc}>{problem.title}</span>
+                <div className={styles.tag_box}>
+                  {problem.tags.map((tag, index) => (
+                    <div key={index} className={styles.tag}>
+                      {tag}
+                    </div>
+                  ))}
+                </div>
               </div>
-              <span className={styles.desc}>{problem.title}</span>
-              <div className={styles.tag_box}>
-                {problem.tags.map((tag, index) => (
-                  <div key={index} className={styles.tag}>
-                    {tag}
-                  </div>
-                ))}
-              </div>
-            </div>
-          ))}
+            ))}
         </div>
       </div>
     </div>
